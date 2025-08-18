9a39c3dc4e33f84b28f07a71b80f381abf02b25e
harshgg2 <gargharsh2307@gmail.com>
2025-08-18 23:18:07 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter array length ";
+    cin >> n;
+    int arr[100]; 
+    cout << "Enter numbers" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; ) {
+            if (arr[i] == arr[j]) {
+                for (int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--; 
+            } else {
+                j++; 
+            }
+        }
+    }
+
+    cout << "Displaying Array"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+    }
+
+
+    return 0;
+}
