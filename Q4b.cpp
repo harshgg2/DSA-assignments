665d290bb38c8ee4c924210803cf6ae7197880e0
harshgg2 <gargharsh2307@gmail.com>
2025-08-18 23:27:47 +0530

    Update Q4b.cpp

--- Q4b.cpp
+++ Q4b.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int r1, c1, r2, c2;
-    cin >> r1 >> c1 >> r2 >> c2;
-    int A[10][10], B[10][10], C[10][10] = {0};
-    for (int i = 0; i < r1; i++)
-        for (int j = 0; j < c1; j++)
-            cin >> A[i][j];
-    for (int i = 0; i < r2; i++)
-        for (int j = 0; j < c2; j++)
-            cin >> B[i][j];
-    if (c1 == r2)
-    {
-        for (int i = 0; i < r1; i++)
-            for (int j = 0; j < c2; j++)
-                for (int k = 0; k < c1; k++)
-                    C[i][j] += A[i][k] * B[k][j];
-        for (int i = 0; i < r1; i++)
-        {
-            for (int j = 0; j < c2; j++)
-                cout << C[i][j] << " ";
+
+int main() {
+    int rowsA, colsA, rowsB, colsB;
+    cin >> rowsA >> colsA >> rowsB >> colsB;
+
+    int mat1[10][10], mat2[10][10], result[10][10] = {0};
+
+    for (int r = 0; r < rowsA; r++)
+        for (int c = 0; c < colsA; c++)
+            cin >> mat1[r][c];
+
+    for (int r = 0; r < rowsB; r++)
+        for (int c = 0; c < colsB; c++)
+            cin >> mat2[r][c];
+
+    if (colsA == rowsB) {
+        for (int r = 0; r < rowsA; r++) {
+            for (int c = 0; c < colsB; c++) {
+                int sum = 0;
+                for (int k = 0; k < colsA; k++) {
+                    sum += mat1[r][k] * mat2[k][c];
+                }
+                result[r][c] = sum;
+            }
+        }
+
+        for (int r = 0; r < rowsA; r++) {
+            for (int c = 0; c < colsB; c++)
+                cout << result[r][c] << " ";
             cout << "\n";
         }
     }
